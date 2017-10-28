db18f3b5930bc2afce84856d6fa53343bfaeadfa
adamo57 <adamouellette57@gmail.com>
2017-10-27 20:32:10 -0400

    Added ability to have bot start listening

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"github.com/nlopes/slack"
 	"os"
 	"regexp"
+	"log"
 )
+
+var IsPodcastRunning = false
 
 func main() {
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
@@ -17,9 +20,20 @@ func main() {
 		switch ev := msg.Data.(type) {
 
 		case *slack.MessageEvent:
-			match, err := regexp.MatchString("^<", ev.Text)
 
-			if match == true && err == nil {
+			match, err := regexp.MatchString("^<", ev.Text)
+			if err != nil {
+				log.Fatal("error processing string")
+			}
+
+			if ev.Text == "start podcast" {
+				api.PostMessage("general", "I am now listening, type _stop podcast_ " +
+					"to tell me to stop listening and give you a list", slack.NewPostMessageParameters())
+
+				IsPodcastRunning = true
+			}
+
+			if IsPodcastRunning == true && match == true {
 				api.PostMessage("general", ev.Text, slack.NewPostMessageParameters())
 			}
 		}
