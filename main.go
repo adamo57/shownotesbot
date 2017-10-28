f67a6ff0c8f96f5ad0fad9d7c6f78be6f276bce4
adamo57 <adamouellette57@gmail.com>
2017-10-27 21:47:03 -0400

    pulled out podcast status check to new func

--- main.go
+++ main.go
@@ -23,21 +23,27 @@ func main() {
 
 		case *slack.MessageEvent:
 
+			checkPodcastStatus(api, ev.Text)
+
 			match, err := regexp.MatchString("^<", ev.Text)
 			if err != nil {
 				log.Fatal("error processing string")
-			}
-
-			if ev.Text == "start podcast" {
-				api.PostMessage("general", "I am now listening, type _stop podcast_ " +
-					"to tell me to stop listening and give you a list", slack.NewPostMessageParameters())
-
-				IsPodcastRunning = true
 			}
 
 			if IsPodcastRunning == true && match == true {
 				api.PostMessage("general", ev.Text, slack.NewPostMessageParameters())
 			}
 		}
+	}
+}
+
+func checkPodcastStatus(api *slack.Client, podcastStatusText string) {
+	if podcastStatusText == "start podcast" {
+		api.PostMessage("general", "I am now listening, type _stop podcast_ " +
+			"to tell me to stop listening and give you a list", slack.NewPostMessageParameters())
+		IsPodcastRunning = true
+	} else if podcastStatusText == "stop podcast"{
+		api.PostMessage("general", "I am now *not* listening", slack.NewPostMessageParameters())
+		IsPodcastRunning = false
 	}
 }
